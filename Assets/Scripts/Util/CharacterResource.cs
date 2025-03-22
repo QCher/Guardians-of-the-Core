bc06f11130a1167522bb1c236dc9970e8425fddb
QCher <kucherenkov1990@gmail.com>
2025-03-22 15:05:09 +0200

    add characters containers

---
+++ Assets/Scripts/Util/CharacterResource.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using UnityEngine.AddressableAssets;
+
+[Serializable]
+public class CharacterResource
+{
+    public string Id;
+    public AssetReference  Asset;
+}
---
+++ Assets/Scripts/Util/CharactersContainer.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CharactersContainer", menuName = "Scriptable Objects/CharactersContainer")]
+public class CharactersContainer : ScriptableObject
+{
+    
+}
