fbcc8b0d2caed2b9cf4ba0af163847c4482f8157
QCher <kucherenkov1990@gmail.com>
2025-03-22 15:06:19 +0200

    Update CharactersContainer.cs

--- Assets/Scripts/Util/CharactersContainer.cs
+++ Assets/Scripts/Util/CharactersContainer.cs
@@ -1,7 +1,8 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "CharactersContainer", menuName = "Scriptable Objects/CharactersContainer")]
 public class CharactersContainer : ScriptableObject
 {
-    
+    public List<CharacterResource> Characters;
 }
