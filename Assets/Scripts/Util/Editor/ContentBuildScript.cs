0da24f2e5abef95e21cfd2ec5c24286559db9801
QCher <kucherenkov1990@gmail.com>
2025-05-03 13:10:02 +0300

    Update ContentBuildScript.cs

--- Assets/Scripts/Util/Editor/ContentBuildScript.cs
+++ Assets/Scripts/Util/Editor/ContentBuildScript.cs
@@ -1,9 +1,8 @@
-using UnityEditor.AddressableAssets.Build;
 using UnityEditor.AddressableAssets.Build.DataBuilders;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "ContentBuildScript.asset", menuName = "Addressables/Content Builders/ContentBuildScript")]
-public class ContentBuildScript : BuildScriptBase, IDataBuilder
+public class ContentBuildScript : BuildScriptPackedMode
 {
     
 }
