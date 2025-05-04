5f912c0c3262663fab1e4c61355b24f0352d8af7
QCher <kucherenkov1990@gmail.com>
2025-05-04 10:54:20 +0300

    add build with defines example

---
+++ Assets/Scripts/Util/Editor/BuildWithScriptingDefines.cs
@@ -0,0 +1,50 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Util.Editor
+{
+    [InitializeOnLoad]
+    public class BuildWithScriptingDefines
+    {
+        static BuildWithScriptingDefines()
+        {
+            bool toBuild = SessionState.GetBool("BuildAddressables", false);
+            SessionState.EraseBool("BuildAddressables");
+            if (toBuild)
+            {
+                Debug.Log("Domain reload complete, building Addressables as requested");
+                BuildAddressablesAndRevertDefines();
+            }
+        }
+
+        [MenuItem("Build/Addressables with script define")]
+        public static void BuildTest()
+        {
+#if !MYDEFINEHERE
+            Debug.Log("Setting up SessionState to inform an Addressables build is requested on next Domain Reload");
+            SessionState.SetBool("BuildAddressables", true);
+            string originalDefines = PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
+            string newDefines = string.IsNullOrEmpty(originalDefines) ? "MYDEFINEHERE" : originalDefines + ";MYDEFINEHERE";
+            Debug.Log("Setting Scripting Defines, this will then start compiling and begin a domain reload of the Editor Scripts.");
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup, newDefines);
+#endif
+        }
+
+        static void BuildAddressablesAndRevertDefines()
+        {
+#if MYDEFINEHERE
+        Debug.Log("Correct scripting defines set for desired build");
+        AddressableAssetSettings.BuildPlayerContent();
+        string originalDefines = PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
+        if (originalDefines.Contains(";MYDEFINEHERE"))
+            originalDefines = originalDefines.Replace(";MYDEFINEHERE", "");
+        else
+            originalDefines = originalDefines.Replace("MYDEFINEHERE", "");
+        PlayerSettings.SetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup, originalDefines);
+        AssetDatabase.SaveAssets();
+#endif
+            EditorApplication.Exit(0);
+        }
+    }
+
+}
